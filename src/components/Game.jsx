642c822f806be0173acacdf2e428737899d64e9b
Aromal Anil <aromalanilkannan@gmail.com>
2020-04-10 18:47:46 +0530

    Initial Commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import MoleHole from './MoleHole';
+
+const Game=()=>{
+    return(
+        <div className="game-grid">
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+            <MoleHole/>
+        </div>
+    )
+}
+
+export default Game;
---
+++ src/components/MoleHole.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MoleHole=()=>{
+    return(
+        <div className="hole">
+
+        </div>
+    )
+}
+
+export default MoleHole;
